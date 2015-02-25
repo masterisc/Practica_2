e2bd845c85061ba47f68115d44a254d97db1f614
masterisc <ischardware@hotmail.com>
2015-02-25 11:00:45 -0600

    avance

--- Practica2/Program.cs
+++ Practica2/Program.cs
@@ -1,12 +1,37 @@
 using System;
 
-namespace Practica2
-{
-	class MainClass
-	{
-		public static void Main (string[] args)
+namespace Practica1 
+{ 
+	class Program 
+	{ 
+		static void Main(string[] args) 
 		{
-			Console.WriteLine ("Hello World!");
-		}
-	}
+			Console.Title = "Practica2"; 
+		
+			int primero, segundo; 
+			 
+			Console.WriteLine("Programa muestra cronometro y reloj \n\n Selecciona una opcion: \n"); 
+			Console.WriteLine("1= cronometro\n2= Reloj\n"); 
+
+			int opc = int.Parse(Console.ReadLine()); 
+
+			switch (opc) { 
+
+			case 1: 
+				Console.WriteLine ("Cronometro: \n"); 
+
+			break;
+
+			case 2: 
+
+				Console.WriteLine("Reloj: \n"); 
+				Console.Write("\t Dame el primer numero: "); 
+			
+			break; 
+
+			} 
+
+			Console.ReadKey(true);
+		} 
+	} 
 }
